af9fb52cbda884a7eaabc922553b8180f561ad95
Connor Yukna <connoryukna@mwc-028143.dhcp.missouri.edu>
2018-09-14 15:35:31 -0500

    First commit

---
+++ Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Dog_Challenge
+{
+    public class Dog
+    {
+        public enum Gender
+        {
+            Male,Female,
+        }
+
+        public string name;
+        public string owner;
+        public int age;
+        public Gender gender;
+
+        public Dog(string n, string o, int a, Gender g)
+        {
+            name = n;
+            owner = o;
+            age = a;
+            gender = g;
+        }
+
+        public void bark(int num)
+        {
+            for (int i = 0; i < num;i++)
+            {
+                Console.Write("Woof!");
+            }
+            Console.WriteLine();
+        }
+
+        public static string checkGender(Gender gender)
+        {
+            if (gender == Gender.Male)
+            {
+                return "His";
+            }
+            else
+            {
+                return "Hers";
+            }
+        }
+
+        public string getTag()
+        {
+            string check = checkGender(gender).ToString();
+
+            if(check=="His")
+            {
+                check = "he";
+            }
+            else
+            {
+                check = "she";
+            }
+
+            return "If lost, call " + owner + "." + check + "name is" + name + "and" + check + "is" + age + "old.";
+
+        }
+
+    }
+
+}
